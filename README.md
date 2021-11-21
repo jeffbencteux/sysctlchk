d8f8e7cc0aed6e1e5ec09f16f896f5e50c28bd5c
Jeffrey Bencteux <jeffbencteux>
2021-11-21 19:58:27 +0100

    add script and doc

---
+++ README.md
@@ -0,0 +1,61 @@
+# README
+
+```
+Usage: ./sysctlchk.sh [OPTIONS]...
+Check sysctl values against a reference file.
+
+Arguments:
+  -b print only failed entries
+  -f reference file, format is as the 'sysctl -a' output
+  -h display this help and exit
+  -l log file to output to
+  -v verbose mode
+```
+
+A simple, POSIX shell script to check sysctl values against a
+predefined reference file.
+
+Some reference files are available under refs/, note that these lists
+are likely to change and are not exhaustive. You are encouraged to
+make your own(s).
+
+Current lists are:
+
+* all.conf (default): hardening settings for network, kernel and
+  filesystem that have a reasonable impact on performance and
+  operations.
+* harder.conf: hardening settings for network, kernel and filesystem that
+  either have a big or unknown impact on performances, has not been
+  tested yet or is only available on recent kernels.
+
+In time, it is likely that settings from harder.conf migrates to
+all.conf.
+
+Note that root permissions are needed to query specific sysctl values
+but script will run even as a non-privileged user, displaying "sysctl:
+permission denied on key" errors.
+
+Sources for the lists - kudos to all of the authors:
+
+* [Kernel documentation](https://www.kernel.org/doc/Documentation/sysctl/)
+* [ANSSI guide for Linux hardening](https://www.kernel.org/doc/Documentation/sysctl/)
+* [Arch Linux wiki](https://wiki.archlinux.org/title/Security#Kernel_hardening) and also [here](https://wiki.archlinux.org/title/sysctl#TCP/IP_stack_hardening)
+
+## Examples
+
+Default list, with output log:
+
+```
+./sysctlchk.sh -l sysctlchk.log
+```
+
+![example 1](img/1.png "Example 1")
+
+Custom reference file, verbose, only displaying failed checks, with
+ouput log:
+
+```
+./sysctlchk.sh -l sysctlchk.log -f /some/custom/file.conf -b -v
+```
+
+![example 2](img/2.png "Example 2")
---
+++ sysctlchk.sh
@@ -0,0 +1,126 @@
+#!/bin/sh
+# Copyright (c) 2021, Jeffrey Bencteux
+# All rights reserved.
+
+# This source code is licensed under the GPLv3 license found in the
+# LICENSE file in the root directory of this source tree.
+
+usage()
+{
+    echo "Usage: $0 [OPTIONS]..."
+    echo "Check sysctl values against a reference file."
+    echo
+    echo "Arguments:"
+    echo "  -b print only failed entries"
+    echo "  -f reference file, format is as the 'sysctl -a' output"
+    echo "  -h display this help and exit"
+    echo "  -l log file to output to"
+    echo "  -v verbose mode"
+    exit 0
+}
+
+log()
+{
+    echo "$1"
+
+    if [ "$logging" -eq 1 ]; then
+	echo "$1" >> "$l"
+    fi
+}
+
+v=0
+b=0
+logging=0
+f="refs/all.conf"
+
+while getopts "bf:hl:v" o; do
+    case "${o}" in
+	b)
+	    b=1
+	    ;;
+        f)
+            f=${OPTARG}
+            ;;
+        h)
+            usage
+            ;;
+	l)
+	    logging=1
+	    l=${OPTARG}
+	    ;;
+	v)
+	    v=1
+	    ;;
+        *)
+            usage
+            ;;
+    esac
+done
+shift $((OPTIND-1))
+
+log "  .-')                  .-')              .-') _                       ('-. .-..-. .-')   "
+log " ( OO ).               ( OO ).           (  OO) )                     ( OO )  /\  ( OO )  "
+log "(_)---\_)  ,--.   ,--.(_)---\_)   .-----./     '._ ,--.       .-----. ,--. ,--.,--. ,--.  "
+log "/    _ |    \  \`.'  / /    _ |   '  .--./|'--...__)|  |.-')  '  .--./ |  | |  ||  .'   /  "
+log "\  :\` \`.  .-')     /  \  :\` \`.   |  |('-.'--.  .--'|  | OO ) |  |('-. |   .|  ||      /,  "
+log " '..\`''.)(OO  \   /    '..\`''.) /_) |OO  )  |  |   |  |\`-' |/_) |OO  )|       ||     ' _) "
+log ".-._)   \ |   /  /\_  .-._)   \ ||  |\`-'|   |  |  (|  '---.'||  |\`-'| |  .-.  ||  .   \   "
+log "\       / \`-./  /.__) \       /(_'  '--'\   |  |   |      |(_'  '--'\ |  | |  ||  |\   \  "
+log " \`-----'    \`--'       \`-----'    \`-----'   \`--'   \`------'   \`-----' \`--' \`--'\`--' '--'  "
+log
+log ""
+
+
+if [ ! -e "$f" ]; then
+    echo "Input file $f does not exists"
+    usage
+fi
+
+log "Loading reference file $f"
+
+if [ $logging -eq 1 ]; then
+    log "Logging into $l"
+fi
+
+log ""
+
+good=0
+bad=0
+
+while read -r line; do
+    # ignoring empty lines
+    if [ "$line" = "" ]; then
+	continue
+    fi
+
+    # ignoring comments
+    if echo "$line" | grep -qE '^#'; then
+	continue
+    fi
+
+    refname=$(echo "$line" | cut -d' ' -f1)
+    cur=$(sysctl "$refname")
+
+    if [ "$line" = "$cur" ]; then
+	good=$((good + 1))
+	if [ $b -eq 1 ]; then
+	    continue
+	fi
+
+	log "\e[1;32m[+]\e[0m $refname"
+    else
+	bad=$((bad + 1))
+	log "\e[1;31m[-]\e[0m $refname"
+    fi
+
+    if [ $v -eq 1 ]; then
+	log "reference: $line"
+	log "current  : $cur"
+    fi
+done < "$f"
+
+log  ""
+log "Statistics:"
+log "  $good passed"
+log "  $bad failed"
+log "  $((good + bad)) total"
